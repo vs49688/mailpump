9563f6c62d0ba78545e4c22206b00b8bc829e7f8
Zane van Iperen <zane@zanevaniperen.com>
2021-12-22 01:09:06 +1000

    imap/persistentclient: add test cases

---
+++ imap/persistentclient/persistentclient_test.go
@@ -0,0 +1,56 @@
+package persistentclient
+
+import (
+	log "github.com/sirupsen/logrus"
+	"github.com/stretchr/testify/assert"
+	"github.com/vs49688/mailpump/imap"
+	"testing"
+	"time"
+)
+
+func TestIdleCancellation(t *testing.T) {
+	log.SetLevel(log.TraceLevel)
+	f := &Factory{}
+
+	c, err := f.NewClient(&imap.ClientConfig{
+		HostPort:  "0.0.0.0:993",
+		Username:  "username",
+		Password:  "password",
+		TLS:       false,
+		TLSConfig: nil,
+		Debug:     false,
+		Updates:   nil,
+	})
+	assert.NoError(t, err)
+	ch := make(chan error)
+
+	go func() { ch <- c.Idle(nil, nil) }()
+	time.Sleep(5 * time.Second)
+	err = c.Logout()
+	assert.NoError(t, err)
+
+	err = <-ch
+	assert.Error(t, err)
+}
+
+func TestIdleAfterLogout(t *testing.T) {
+	log.SetLevel(log.TraceLevel)
+	f := &Factory{}
+
+	c, err := f.NewClient(&imap.ClientConfig{
+		HostPort:  "0.0.0.0:993",
+		Username:  "username",
+		Password:  "password",
+		TLS:       false,
+		TLSConfig: nil,
+		Debug:     false,
+		Updates:   nil,
+	})
+	assert.NoError(t, err)
+
+	err = c.Logout()
+	assert.NoError(t, err)
+
+	err = c.Idle(nil, nil)
+	assert.Error(t, err)
+}

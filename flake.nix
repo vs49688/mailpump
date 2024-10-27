dbcecc95cf06ee50fe88214e4f1ca19471002bce
Zane van Iperen <zane@zanevaniperen.com>
2024-10-28 01:14:29 +0900

    flake: remove static & ci targets

--- flake.nix
+++ flake.nix
@@ -19,36 +19,9 @@
     };
 
     packages = forAllSystems (pkgs: rec {
-      mailpump = pkgs.callPackage ./default.nix {
+      default = pkgs.callPackage ./default.nix {
         version = self.lastModifiedDate;
       };
-
-      mailpump-static = mailpump.overrideAttrs(old: {
-        ldflags     = [ "-s" "-w" ];
-        CGO_ENABLED = 0;
-      });
-
-      default = mailpump;
-
-      ci = pkgs.stdenvNoCC.mkDerivation (finalAttrs: {
-        inherit (mailpump) version;
-
-        pname = "mailpump-ci";
-
-        dontUnpack = true;
-
-        installPhase = ''
-          mkdir -p $out
-
-          cp "${mailpump-static}/bin/mailpump" \
-            "$out/mailpump-${finalAttrs.version}-${mailpump-static.stdenv.hostPlatform.system}"
-          chmod 0755 "$out/mailpump-${finalAttrs.version}-${mailpump-static.stdenv.hostPlatform.system}"
-
-          cd "$out" && for i in *; do
-            sha256sum -b "$i" > "$i.sha256"
-          done
-        '';
-      });
     });
   };
 }
